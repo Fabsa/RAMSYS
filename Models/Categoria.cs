778f17f01275a75d05dc2a3662c24902455d6665
Fabio Ferreira <103062036+Kinadourado@users.noreply.github.com>
2022-11-02 16:34:06 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAMSYS.Models
+{
+    public class Categoria
+    {
+
+
+        public Categoria()
+        {
+           
+        }       
+    }
+
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAMSYS.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome{ get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public string PurchaseOrder { get; set; }        
+        public int QuantiMin { get; set; }
+        public string Locacao { get; set; }
+        public string Destino { get; set; }
+        public string UserId { get; set; }
+        
+
+    }
+}
